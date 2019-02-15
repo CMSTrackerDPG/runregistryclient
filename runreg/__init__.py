311abc9ed2bb906bb354900e9309f519167efecd
Peter Stein <peterstein@protonmail.com>
2019-02-15 03:42:33 +0100

    Add GPL license to file headers

--- runreg/__init__.py
+++ runreg/__init__.py
@@ -1,3 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# © Copyright 2019 CERN
+#
+# This software is distributed under the terms of the GNU General Public
+# Licence version 3 (GPL Version 3), copied verbatim in the file “LICENSE”
+#
+# In applying this licence, CERN does not waive the privileges and immunities
+# granted to it by virtue of its status as an Intergovernmental Organization
+# or submit itself to any jurisdiction.
+
 from .client import get
 
 __version__ = "0.0.1"
